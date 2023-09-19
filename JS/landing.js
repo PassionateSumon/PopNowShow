e16dc42db0203d931945e832f5ad3395125b6126
Sumon Mitra <sumonmitra.21@gmail.com>
2023-09-19 23:18:07 +0530

    Updates of codes

---
+++ JS/landing.js
@@ -0,0 +1,19 @@
+const search_icon_access = document.querySelector('#search_icon');
+const search_bar_access = document.querySelector('#search');
+
+let flag = false
+search_icon_access.addEventListener('click', () => {
+    const searchBar = document.createElement('input')
+    searchBar.id = 'search_bar'
+    searchBar.type = 'search'
+    searchBar.placeholder = 'Enter Movie Name'
+    searchBar.style.transition = '0.3s'
+    if(flag == false) {
+        search_bar_access.appendChild(searchBar)
+        flag = true;
+    } else {
+        const s = document.querySelector('#search_bar')
+        s.remove()
+        flag = false;
+    }
+})
