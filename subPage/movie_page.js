6b4aaff01e7bce2111f5c8d440b6a212e574cc50
Saikat Das <saikatdas40g@gmail.com>
2023-10-27 15:06:36 +0530

    video controls update

--- subPage/movie_page.js
+++ subPage/movie_page.js
@@ -1,8 +1,9 @@
 let trailer_middle_pause = document.querySelector('.fa-circle-play');
+let thumbnail = document.querySelector('.thumbnail');
 let trailer_customize = document.querySelector('.trailer_customize');
 trailer_middle_pause.addEventListener('click', () => {
-    // console.log(trailer_customize);
-    if(trailer_customize.play()) console.log("playing");
+    trailer_customize.play();
+    thumbnail.style.display = 'none'
     trailer_customize.setAttribute("controls","");
     trailer_middle_pause.classList.remove('fa-circle-play');
 })
