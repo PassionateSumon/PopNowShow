686ac5e40ce321346c14f4eb09a3e595641d2769
Saikat Das <saikatdas40g@gmail.com>
2023-10-27 23:03:18 +0530

    cinema added

--- subPage/movie_page.js
+++ subPage/movie_page.js
@@ -4,6 +4,6 @@ let trailer_customize = document.querySelector('.trailer_customize');
 trailer_middle_pause.addEventListener('click', () => {
     trailer_customize.play();
     thumbnail.style.display = 'none'
-    trailer_customize.setAttribute("controls","");
+    trailer_customize.setAttribute("controls", "");
     trailer_middle_pause.classList.remove('fa-circle-play');
 })
