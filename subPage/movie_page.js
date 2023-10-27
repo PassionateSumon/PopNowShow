d008863a8927fbf60352ce52a9e5e108f5137fcb
Sumon Mitra <sumonmitra.21@gmail.com>
2023-10-27 14:53:45 +0530

    abc

--- subPage/movie_page.js
+++ subPage/movie_page.js
@@ -1,5 +1,4 @@
 let trailer_middle_pause = document.querySelector('.fa-circle-play');
 trailer_middle_pause.addEventListener('click', ()=> {
     trailer_middle_pause.classList.toggle('fa-circle-pause');
-    console.log("abc");
 })
